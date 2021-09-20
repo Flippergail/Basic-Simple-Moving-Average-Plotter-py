f53a68941657fe9b78e247ab5f356b6440abc574
Flippergail <ticarter11@icloud.com>
2021-09-20 19:38:16 +0100

    moving file location of github

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 
 # Getting the data by specifying the stock ticker, start date, and end date
-data = yf.download('^SP500TR','2020-09-01','2021-09-01')
+data = yf.download('^SP500TR', '2020-09-01', '2021-09-01')
 # Calculating returns
 mdata = data.resample('M').apply(lambda x: x[-1])
 monthly_return = mdata['Adj Close'].pct_change()
